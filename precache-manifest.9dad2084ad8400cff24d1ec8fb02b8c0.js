839005093cbc592dac78480f027d5faa89748b95
Mas4erLomas4er <i9997117@gmail.com>
2018-11-25 15:54:24 +0300

    Initial commit

---
+++ precache-manifest.9dad2084ad8400cff24d1ec8fb02b8c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/static/media/polygon.cc54c19b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e68c7d9b298d4d664a73",
+    "url": "/static/js/main.e68c7d9b.chunk.js"
+  },
+  {
+    "revision": "1f4cb1c9d377aa1f180b",
+    "url": "/static/js/1.1f4cb1c9.chunk.js"
+  },
+  {
+    "revision": "e68c7d9b298d4d664a73",
+    "url": "/static/css/main.cfe489e2.chunk.css"
+  },
+  {
+    "revision": "1f4cb1c9d377aa1f180b",
+    "url": "/static/css/1.bc4593e2.chunk.css"
+  },
+  {
+    "revision": "3007ab2e09d6dd43be1eff9020bbfaae",
+    "url": "/index.html"
+  }
+];
